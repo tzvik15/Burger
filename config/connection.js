454ad5e32e2311bac020b29d3fea18d8d3235fbd
tzvik15 <tzvik15@yahoo.com>
2019-12-09 09:29:15 -0700

    added JAWSDB environment variable check to the connection file

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,26 @@ require("dotenv").config();
 
 var PORT = process.env.PORT || 8080;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  database: "employees_DB"
-});
+let connection;
+
+
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: "employees_DB"
+  });
+
+}
+
+
+
+
 
 
 connection.connect(function(err) {
